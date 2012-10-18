72ef193658d1de1d2439a0e98abd1dc6dfb84608
Ashwin Purohit <purohit@gmail.com>
2012-10-17 17:27:38 -0700

    Interface doesn't need that much typecasting

--- app.go
+++ app.go
@@ -9,7 +9,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-    "strings"
+	"strings"
 )
 
 // To parse the JSON responses
@@ -44,67 +44,78 @@ type NamesResponse struct {
 	Profiles  []ProfileNameResponse `json:"profiles"`
 }
 
-func buildConfig() map[string]interface{} {
+type ProfileGenotypeResponse struct {
+	Id        string `json:"id"`
+	rs9525638 string
+	rs2908004 string
+	rs2707466 string
+	rs7776725 string
+}
+
+type GenotypeResponse struct {
+	Profiles []ProfileGenotypeResponse
+}
+
+func buildConfig() map[string]string {
 	c, _ := config.ReadDefault("config.cfg")
-	configs := make(map[string]interface{})
+	configs := make(map[string]string)
 	section := "DEFAULT"
 	/*cortical_thickness_snps := []string{"rs9525638", "rs2707466"}*/
 	/*forearm_bmd_snps := []string{"rs2536189", "rs2908004", "rs2707466"}*/
 	/*forearm_fracture_snps := []string{"rs7776725", "rs2908004", "rs2707466"}*/
-    scopes := []string{"rs9525638", "rs2908004", "rs2707466", "rs7776725", "rs2908004", "basic", "names"}
-    configs["scope"] = strings.Join(scopes, " ")
+	genotype_scopes := []string{"rs9525638", "rs2908004", "rs2707466", "rs7776725"}
+	regular_scopes := []string{"basic", "names"}
+	scopes := make([]string, len(genotype_scopes)+len(regular_scopes))
+	copy(scopes, regular_scopes)
+	copy(scopes[len(regular_scopes):], genotype_scopes)
+	configs["genotype_scopes"] = strings.Join(genotype_scopes, " ")
+	configs["scope"] = strings.Join(scopes, " ")
 	// Your API credentials and server info
-	config_string_keys := []string{"client_id", "client_secret", "api_uri", "redirect_uri",
-		"cookie_secret", "static_path", "session_name", "session_access_token_key"}
-	config_int_keys := []string{"port"}
+	config_keys := []string{"client_id", "client_secret", "api_uri", "redirect_uri",
+		"cookie_secret", "static_path", "session_name", "session_access_token_key",
+		"port"}
 
 	var err error
-	for _, value := range config_string_keys {
+	for _, value := range config_keys {
 		configs[value], err = c.String(section, value)
 		if err != nil {
 			log.Fatal("You must define %s in your config file", value)
 		}
 	}
-	for _, value := range config_int_keys {
-		configs[value], err = c.Int(section, value)
-		if err != nil {
-			log.Fatal("You must define %s in your config file", value)
-		}
-	}
-    return configs
+	return configs
 }
 
 func main() {
 	config := buildConfig()
-    store := sessions.NewCookieStore([]byte(config["cookie_secret"].(string)))
+	store := sessions.NewCookieStore([]byte(config["cookie_secret"]))
 
 	templates := template.Must(template.ParseFiles("templates/index.dtml", "templates/result.dtml"))
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(config["static_path"].(string)))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(config["static_path"]))))
 	http.HandleFunc("/receive_code/", func(w http.ResponseWriter, req *http.Request) {
-        receiveCode(w, req, config, store, templates)
-    })
+		receiveCode(w, req, config, store, templates)
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-        index(w, req, config, store, templates)
-    })
-	err := http.ListenAndServe(fmt.Sprintf(":%d", config["port"].(int)), nil)
+		index(w, req, config, store, templates)
+	})
+	err := http.ListenAndServe(":"+config["port"], nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
 
-func receiveCode(w http.ResponseWriter, req *http.Request, config map[string]interface{}, store *sessions.CookieStore, templates *template.Template) {
-	session, _ := store.Get(req, config["session_name"].(string))
+func receiveCode(w http.ResponseWriter, req *http.Request, config map[string]string, store *sessions.CookieStore, templates *template.Template) {
+	session, _ := store.Get(req, config["session_name"])
 	context, _ := url.ParseQuery(req.URL.RawQuery)
 	if code, ok := context["code"]; ok {
 		auth_code := string(code[0])
-		resp, _ := http.PostForm(config["api_uri"].(string)+"/token/",
-			url.Values{"client_id": {config["client_id"].(string)},
-				"client_secret": {config["client_secret"].(string)},
+		resp, _ := http.PostForm(config["api_uri"]+"/token/",
+			url.Values{"client_id": {config["client_id"]},
+				"client_secret": {config["client_secret"]},
 				"grant_type":    {"authorization_code"},
 				"code":          {auth_code},
-				"redirect_uri":  {config["redirect_uri"].(string)},
-				"scope":         {config["scope"].(string)},
+				"redirect_uri":  {config["redirect_uri"]},
+				"scope":         {config["scope"]},
 			})
 		defer resp.Body.Close()
 		if resp.StatusCode == 200 {
@@ -114,33 +125,31 @@ func receiveCode(w http.ResponseWriter, req *http.Request, config map[string]int
 			if err != nil {
 				log.Printf(err.Error())
 			} else {
-				session.Values[config["session_access_token_key"].(string)] = t_res.AccessToken
+				session.Values[config["session_access_token_key"]] = t_res.AccessToken
 				session.Save(req, w)
 				log.Printf("token is %s", t_res.AccessToken)
 				http.Redirect(w, req, "/", 303)
 			}
 		}
 	} else if error_type, ok := context["error"]; ok {
-		error_type := string(error_type[0])
-		error_description := string(context["error_description"][0])
-		fmt.Fprintf(w, "Failed: %s: %s", error_type, error_description)
+		fmt.Fprintf(w, "%s: %s", string(error_type[0]), string(context["error_description"][0]))
 	}
 }
 
-func index(w http.ResponseWriter, req *http.Request, config map[string]interface{}, store *sessions.CookieStore, templates *template.Template) {
-	session, _ := store.Get(req, config["session_name"].(string))
-	access_token, ok := session.Values[config["session_access_token_key"].(string)].(string)
+func index(w http.ResponseWriter, req *http.Request, config map[string]string, store *sessions.CookieStore, templates *template.Template) {
+	session, _ := store.Get(req, config["session_name"])
+	access_token, ok := session.Values[config["session_access_token_key"]].(string)
 	if !ok {
 		context := map[string]string{
 			"path":         req.URL.Path,
-			"client_id":    config["client_id"].(string),
-			"scope":       config["scope"].(string),
-			"redirect_uri": config["redirect_uri"].(string),
+			"client_id":    config["client_id"],
+			"scope":        config["scope"],
+			"redirect_uri": config["redirect_uri"],
 		}
 		_ = templates.ExecuteTemplate(w, "index.dtml", context)
 	} else {
 		client := &http.Client{}
-        api_uri := config["api_uri"].(string)
+		api_uri := config["api_uri"]
 
 		req, err := http.NewRequest("GET", api_uri+"/1/user/", nil)
 		req.Header.Add("Authorization", "Bearer "+access_token)
@@ -156,11 +165,16 @@ func index(w http.ResponseWriter, req *http.Request, config map[string]interface
 		dec = json.NewDecoder(resp.Body)
 		err = dec.Decode(&n_res)
 
+		req, err = http.NewRequest("GET", api_uri+"/1/genotype/?locations="+config["genotype_scopes"], nil)
+		req.Header.Add("Authorization", "Bearer "+access_token)
+		resp, err = client.Do(req)
+		var g_res GenotypeResponse
+		dec = json.NewDecoder(resp.Body)
+		err = dec.Decode(&g_res)
+
 		if err != nil {
 			log.Printf(err.Error())
 		} else {
-			log.Printf("user id is %s", u_res.Id)
-			log.Printf("names are %s %s", n_res.FirstName, n_res.LastName)
 		}
 		err = templates.ExecuteTemplate(w, "result.dtml", nil)
 	}
